246374edad9a27c8435c5ac0433545294310b369
Sayem92 <sayemmiha123@gmail.com>
2024-03-08 12:45:10 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Login from './components/Form/Login';
 import Register from './components/Form/Register';
 
 function App() {
   return (
     <div>
-    {/* <Login></Login> */}
     <Register></Register>
     </div>
   );
